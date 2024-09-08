da11c775f6d35351e5585d6edd339f89c50fb09d
yacson3287 <yacson.ramirez@gmail.com>
2024-09-07 23:05:26 -0400

    first commit

---
+++ src/test/java/com/linktic/order_management_service/OrderManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.order_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
